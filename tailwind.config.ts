b9babd7a7e8a58801f65b71a5e626de67da28aff
Habeeb <habeebahmadu1@gmail.com>
2024-06-19 22:50:40 +0100

    feat: add template component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,10 +72,18 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        load: {
+          from: {
+            opacity: "0",
+            transform: "translateY(5rem)",
+          },
+          to: { opacity: "1", transform: "translateY(0%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        load: "load 750ms ease-in-out",
       },
     },
   },
